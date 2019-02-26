7dfaa542e00f38abd24f7a8cb6d8c50cb10e2961
JosueDurand96 <josue.durand8@gmail.com>
2019-02-26 15:11:36 -0500

    detalle noticias

--- lib/noticias.dart
+++ lib/noticias.dart
@@ -35,27 +35,56 @@ class _PaginaNoticiasState extends State<PaginaNoticias> {
         child: ListView.builder(
           itemCount:data==null ? 0 : data.length,
             itemBuilder: (BuildContext context,i){
-            return Card(
-              child: Column(
-                children: <Widget>[
-                  Stack(
-                    children: <Widget>[
-                      Center(child: CircularProgressIndicator(),),
-                      Center(child: Image.network("http://192.168.1.60/fixture/public/img/noticias/"+data[i]['urlfoto']
-                      ),
-                      ),
-                    ],
-                  ),
-                  Text(data[i]['titulo'],style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(data[i]['updated_at']),
-                  ),
-                ],
+            return InkWell(
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        Center(child: CircularProgressIndicator(),),
+                        Center(child: Image.network("http://192.168.1.60/fixture/public/img/noticias/"+data[i]['urlfoto']
+                        ),
+                        ),
+                      ],
+                    ),
+                    Text(data[i]['titulo'],style: TextStyle(fontSize: 20),textAlign: TextAlign.center,),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(data[i]['updated_at']),
+                    ),
+                  ],
+                ),
               ),
+              onTap: (){
+               Navigator.push(context, MaterialPageRoute(builder: (BuildContext context)=> PaginaDetalle(data[i]))
+               );
+              },
             );
             }
         ),
+      ),
+    );
+  }
+}
+
+class PaginaDetalle extends StatelessWidget {
+  PaginaDetalle(this.data);
+  final data;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(data["titulo"]),),
+      body: ListView(
+        children: <Widget>[
+          Image.network("http://192.168.1.60/fixture/public/img/noticias/"+data['urlfoto']),
+          Container(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(data['descripcion'],textAlign: TextAlign.justify,),
+          ),
+          Container(
+            child: Text("Publicado el "+data["updated_at"],textAlign: TextAlign.right,),
+          )
+        ],
       ),
     );
   }
